31a2caa7b7246b7cb4c97f242dcef18a0f2949fe
Hwangseoeun <sunny030921@gmail.com>
2024-09-05 22:57:00 +0900

    feat: Car 객체 생성 및 경주할 자동차의 이름을 입력받는 기능 구현
    최대한 하나의 메서드에서 하나의 동작만 할 수 있도록 inputCarNames와 parseCarNamesToArray로 분리해서 구현함
    
    1. inputCarNames
    사용자가 경주할 자동차의 이름을 입력할 수 있도록 구현
    
    2. parseCarNamesToArray
    입력받은 자동차들의 이름을 파싱을 통해 배열에 저장하도록 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CarService carService = new CarService();
+
+        String carNames = carService.inputCarNames();
+
+        List<Car> cars = carService.parseCarNamesToArray(carNames);
     }
 }
---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public class Car {
+    private final String carName;
+    private int count;
+
+    public Car(String carName){
+        this.carName = carName;
+        this.count = 0;
+    }
+
+    public String getCarName(){
+        return carName;
+    }
+
+    public int getCount(){
+        return count;
+    }
+}
