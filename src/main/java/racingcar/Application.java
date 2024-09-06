58dfb785b74a13e8891eaa4461cb5d0b8b6fdfcb
Hwangseoeun <sunny030921@gmail.com>
2024-09-06 16:08:29 +0900

    refactor: Application 내 메서드 호출 삭제 Application 내에서 메서드를 호출하는 것보다 Controller 클래스를 생성하여 해당 파일에서 메서드를 호출하고 관리하는 것이 나을 것 같아 이동함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,17 +1,9 @@
 package racingcar;
 
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
-        RacingService racingService = new RacingService();
+        RacingController racingController = new RacingController();
 
-        racingService.saveCarNames();
-
-        Integer numberOfTries = racingService.inputNumberOfTries();
-
-        racingService.decideAdvance();
-
-        racingService.chooseRandomCars();
+        racingController.start();
     }
 }
