63f8269613e3841b6d60cc0879a8a0523fc8470b
Hwangseoeun <sunny030921@gmail.com>
2024-09-07 20:26:50 +0900

    refactor: Car 객체의 advanceCount 초기값 상수 선언 기존 advanceCount의 초기값은 매직 넘버인 0으로 선언되어있었음 해당 값이 무엇을 의미하는지 빠르게 파악할 수 있도록 상수 'DEFAULT_ADVANCE_COUNT'로 초기값 선언

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,13 @@
 package racingcar;
 
 public class Car {
+    private static final int DEFAULT_ADVANCE_COUNT = 0;
     private final String name;
     private int advanceCount;
 
     public Car(String name) {
         this.name = name;
-        this.advanceCount = 0;
+        this.advanceCount = DEFAULT_ADVANCE_COUNT;
     }
 
     public String getCarName() {
